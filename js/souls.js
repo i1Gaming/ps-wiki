ec0106837ed4330124af3368e255b75891a7c276
IvanReznichenko <cossacki1vn@gmail.com>
2021-01-12 22:18:35 +0200

    add badges, mob pages

--- js/souls.js
+++ js/souls.js
@@ -1,22 +1,127 @@
+function sortSouls(souls) {
+    souls.sort(function (a, b) {
+        if (a.name > b.name) {
+            return 1;
+        }
+        if (a.name < b.name) {
+            return -1;
+        }
+        return 0;
+    })
+
+    return souls
+};
+
+function getSoulBadges(mobLocationAll) {
+    var badges = [];
+    $.each(mobLocationAll, function (key, location) {
+        var locationRU = '';
+        switch (location) {
+            case 'muldian':
+                locationRU = 'Мальдия'
+                break;
+            case 'el-bed':
+                locationRU = 'Эль Ведд'
+                break;
+            case 'east-road':
+                locationRU = 'Восточная дорога ветров'
+                break;
+            case 'north-road':
+                locationRU = 'Северная дорога ветров'
+                break;
+            case 'west-road':
+                locationRU = 'Западная дорога ветров'
+                break;
+            case 'gorda':
+                locationRU = 'Горда'
+                break;
+            case 'nelstadt':
+                locationRU = 'Долина Нельштадт'
+                break;
+            case 'himurartu':
+                locationRU = 'Пустыня Химурарту'
+                break;
+            case 'gorge':
+                locationRU = 'Лейбгольские Теснины'
+                break;
+            case 'montori':
+                locationRU = 'Долина Монтори'
+                break;
+            case 'tambrin':
+                locationRU = 'Север великой Тамбринской Пустыни'
+                break;
+            default:
+                break;
+        }
+        var locationHTML = "<div class='bg-" + location + " badges' title='" + locationRU + "'></div>"
+
+        badges.push(locationHTML)
+    })
+
+    return badges;
+};
+
+function renderSoul(color, soulItem) {
+    switch (color) {
+        case 'aqua':
+            $('<div/>', {
+                "class": "row",
+                html: soulItem
+            }).appendTo(".aqua-souls");
+            break;
+        case 'black':
+            $('<div/>', {
+                "class": "row",
+                html: soulItem
+            }).appendTo(".black-souls");
+            break;
+        case 'blue':
+            $('<div/>', {
+                "class": "row",
+                html: soulItem
+            }).appendTo(".blue-souls");
+            break;
+        case 'green':
+            $('<div/>', {
+                "class": "row",
+                html: soulItem
+            }).appendTo(".green-souls");
+            break;
+        case 'purple':
+            $('<div/>', {
+                "class": "row",
+                html: soulItem
+            }).appendTo(".purple-souls");
+            break;
+        case 'red':
+            $('<div/>', {
+                "class": "row",
+                html: soulItem
+            }).appendTo(".red-souls");
+            break;
+        case 'yellow':
+            $('<div/>', {
+                "class": "row",
+                html: soulItem
+            }).appendTo(".yellow-souls");
+            break;
+        default:
+            break;
+    }
+}
+
 function getSouls() {
     $.getJSON("../js/souls.json", function (data) {
-        var souls = data.souls;
-        souls.sort(function(a,b) {
-            if (a.name > b.name) {
-                return 1;
-            }
-            if (a.name < b.name) {
-                return -1;
-            }
-            return 0; 
-        })
+        var souls = sortSouls(data.souls);
 
         $.each(souls, function (key, soul) {
             var mobs = [];
             var soulItem = '';
 
             $.each(soul.drop, function (key, mob) {
-                var curMob = "<a class='text-secondary' href='../pages/locations/" + mob.mobLocation + "/" + mob.mobLink + "'>" + mob.mobName + " (" + mob.mobLvl + " ур.) </a> <br>";
+                var badges = getSoulBadges(mob.mobLocationAll);
+
+                var curMob = "<div class='d-flex align-items-center mb-2'><a class='text-secondary' href='../pages/locations/" + mob.mobLocationMain + "/" + mob.mobLink + "'>" + mob.mobName + " (" + mob.mobLvl + " ур.) </a>" + badges.join("") + "</div>";
                 mobs.push(curMob);
             })
 
@@ -31,52 +136,7 @@ function getSouls() {
                 "<li class ='list-group-item col-3'>" + mobs.join("") + "</li>" +
                 "</ul>";
 
-            switch (soul.color) {
-                case 'aqua':
-                    $('<div/>', {
-                        "class": "row",
-                        html: soulItem
-                    }).appendTo(".aqua-souls");
-                    break;
-                case 'black':
-                    $('<div/>', {
-                        "class": "row",
-                        html: soulItem
-                    }).appendTo(".black-souls");
-                    break;
-                case 'blue':
-                    $('<div/>', {
-                        "class": "row",
-                        html: soulItem
-                    }).appendTo(".blue-souls");
-                    break;
-                case 'green':
-                    $('<div/>', {
-                        "class": "row",
-                        html: soulItem
-                    }).appendTo(".green-souls");
-                    break;
-                case 'purple':
-                    $('<div/>', {
-                        "class": "row",
-                        html: soulItem
-                    }).appendTo(".purple-souls");
-                    break;
-                case 'red':
-                    $('<div/>', {
-                        "class": "row",
-                        html: soulItem
-                    }).appendTo(".red-souls");
-                    break;
-                case 'yellow':
-                    $('<div/>', {
-                        "class": "row",
-                        html: soulItem
-                    }).appendTo(".yellow-souls");
-                    break;
-                default:
-                    break;
-            }
+            renderSoul(soul.color, soulItem);
         });
     })
 };
